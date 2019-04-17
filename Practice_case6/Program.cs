de85a2c353ee4e0fe18eaee6a27662eecced0820
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-17 17:58:14 +0300

    Practice case 6

---
+++ Practice_case6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3 number:");
+            double number1 = double.Parse(Console.ReadLine());
+            double number2 = double.Parse(Console.ReadLine());
+            double number3 = double.Parse(Console.ReadLine());
+            double medium = (number1 + number2 + number3) / 3;
+            Console.WriteLine($"Result: {medium}");
+            Console.ReadKey();
+        }
+    }
+}
