1905d508aa4f9125dfb3fd946b617e2a65eebc2e
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-17 17:28:38 +0300

    check name for variables

---
+++ Practice_case1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uberflu? = 5;
+            int _Identifier = 5;
+            int \u006fIdentifier = 5;
+            int &myVar = 5;
+            int myVariab1le = 5;
+
+        }
+    }
+}
