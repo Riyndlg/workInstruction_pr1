2af1cbcb1785a33a0382ce5bf8fd88347d3065a9
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-17 18:08:37 +0300

    practice case 7

---
+++ Practice case7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            double r;
+            double area;
+            Console.Write("Enter value for R: ");
+            r = double.Parse(Console.ReadLine());
+            area = pi * Math.Pow(r, 2);
+        }
+    }
+}
