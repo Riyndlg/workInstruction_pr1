7c008d6fd3518ed54c8445565139b48eb14d7a89
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-17 17:46:52 +0300

    practice case 3

---
+++ Practice_case3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string case1 = "\nмоя строка 1";
+            string case2 = "\tмоя строка 2";
+            string case3 = "\aмоя строка 3";
+            Console.WriteLine(case1);
+            Console.WriteLine(case2);
+            Console.WriteLine(case3);
+            Console.ReadKey();
+        }
+    }
+}
